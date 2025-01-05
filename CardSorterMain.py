7d92bd8246c924658fe787610c6d4bc7a774a946
Jason Walser <r0flcopt0rz10611@gmail.com>
2025-01-05 14:53:06 -0500

    first commit

---
+++ CardSorterMain.py
@@ -0,0 +1,9 @@
+UserDecision = input("What would you like to do?/n 1) View Decks /n 2) Create Deck /n 3) Delete Deck")
+if UserDecision == "1":
+    print ("user selected one?")
+elif UserDecision == "2":
+    print ("user entered two!")
+elif UserDecision == "3":
+    print ("Cannot Delete decks yet")
+else:
+    print ("Invalid input  " + UserDecision)
